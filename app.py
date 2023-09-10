2ffef0b14532b6a2f757df701a2264a272dfb6b1
Manmeet Singh <manmeet20singh11@gmail.com>
2023-09-10 08:25:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-import streamlit as st
 import os
 import json
 import ee
 
-# Read the service account key from the environment variable
-service_account_key = json.loads(os.environ.get('GEE_SERVICE_ACCOUNT_KEY'))
+# Get the environment variable or default to an empty string
+service_account_key_str = os.environ.get('GEE_SERVICE_ACCOUNT_KEY', '')
 
-# Use the service account for authentication
-credentials = ee.ServiceAccountCredentials(email=service_account_key['client_email'], key_data=service_account_key['private_key'])
-ee.Initialize(credentials)
+# Check if the string is not empty before trying to load JSON
+if service_account_key_str:
+    service_account_key = json.loads(service_account_key_str)
+    # Use the service account for authentication
+    credentials = ee.ServiceAccountCredentials(email=service_account_key['client_email'], key_data=service_account_key['private_key'])
+    ee.Initialize(credentials)
+else:
+    print("GEE_SERVICE_ACCOUNT_KEY is not set!")
+    service_account_key = {}
+
+# import streamlit as st
+# import os
+# import json
+# import ee
+
+# # Read the service account key from the environment variable
+# service_account_key = json.loads(os.environ.get('GEE_SERVICE_ACCOUNT_KEY'))
+
+# # Use the service account for authentication
+# credentials = ee.ServiceAccountCredentials(email=service_account_key['client_email'], key_data=service_account_key['private_key'])
+# ee.Initialize(credentials)
 
 st.title("Visualizing Landsat Data with Earth Engine and Streamlit")
 # Define a function to visualize Landsat data
